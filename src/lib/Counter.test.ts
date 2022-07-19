f90dee21c43cb66d9bdf2bfaa1967f4a73c50681
davipon <davipondev@gmail.com>
2022-07-19 19:40:14 +0800

    style: format Couter.test.ts file

--- src/lib/Counter.test.ts
+++ src/lib/Counter.test.ts
@@ -5,29 +5,28 @@ describe('Test Counter.svelte', async () => {
 	it('Initial counter should be 0', async () => {
 		render(Counter);
 
-		const counter = await screen.findByText('0')
+		const counter = await screen.findByText('0');
 		expect(counter).toBeInTheDocument();
 	});
 	it('Test decrease', async () => {
 		render(Counter);
 
-		const decreaseButton = screen.getByLabelText('Decrease the counter by one')
+		const decreaseButton = screen.getByLabelText('Decrease the counter by one');
 
 		await fireEvent.click(decreaseButton);
 		await fireEvent.click(decreaseButton);
 
-		const counter = await screen.findByText('-2')
+		const counter = await screen.findByText('-2');
 		expect(counter).toBeInTheDocument();
-
 	});
 	it('Test increase', async () => {
 		render(Counter);
 
-		const increaseButton = screen.getByLabelText('Increase the counter by one')
+		const increaseButton = screen.getByLabelText('Increase the counter by one');
 
 		await fireEvent.click(increaseButton);
 
-		const counter = await screen.findByText('1')
+		const counter = await screen.findByText('1');
 		expect(counter).toBeInTheDocument();
 	});
 });
